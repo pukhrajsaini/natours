f5f9ddaf2fc5f8189dbc8a07cf688d92e6664972
pukhraj saini <pukhraj.saini97@gmail.com>
2020-07-16 12:59:54 +0530

    add file to import data from json file to mongodb

---
+++ data/dataImport.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('./../models/tourModel');
+dotenv.config({ path: './../config.env' });
+const port = process.env.PORT | 3000;
+
+
+const DB = process.env.DATABASE;
+
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => {
+  console.log('DB connected successfully');
+})
+
+
+// READ JSON file
+
+const tours = JSON.parse(fs.readFileSync('./tours-simple.json', 'utf-8'));
+
+// IMPORTING data
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data imported successfully');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// DELETING data
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted....');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Calling functions
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
+console.log(process.argv);
+
+
